c8df5578da0bbd32526c67d533951228db31f9f5
Tianyi Sun <suntianyi80@gmail.com>
2019-09-21 02:07:48 +1000

    add background music and ship color

--- Battleship-C/bin/Debug/src/GameResources.cs
+++ Battleship-C/bin/Debug/src/GameResources.cs
@@ -52,7 +52,7 @@ public static class GameResources
 
     private static void LoadMusic()
     {
-        NewMusic("Background", "horrordrone.mp3");
+        NewMusic("Background", "horrordrone.wav");
     }
 
     // <summary>
